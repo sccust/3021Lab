d4633f32e2fffb7769a2ac794cd4a5be4bf07ab7
guxiaodong1987 <guxiaodong1987@126.com>
2015-01-30 13:34:49 +0800

    new package

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,9 +1,41 @@
 package demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo
 {
-   public static void main(String[] args)
-   {
-	 // TODO Auto-generated method stub
-   }
+   public static void main(String[] args) {
+	 String line="";
+	 List<Pair<String,String>> posts=new ArrayList<String>();
+	 //String posts[]={"The weather today is so great!!",""};
+	 
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  try {
+        line = br.readLine();
+        if(line.startsWith("post"))
+        {
+     	 posts.add(line.split(" ")[1]);
+     	 System.out.println("Post:"+" "+System.currentTimeMillis());
+        }
+        else if(line.startsWith("delete"))
+        {
+     	 
+        }
+        else if(line.startsWith("list"))
+        {
+     	 
+        }
+        else if(line.startsWith("search"))
+        {
+     	 
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    System.out.println("readString 方法的输入：" + line);
+}
 }
---
+++ src/main/java/util/data/Pair.java
@@ -0,0 +1,45 @@
+package codemining.util.data;
+
+import com.google.common.base.Objects;
+
+public class Pair<T1, T2> {
+    public final T1 first;
+    public final T2 second;
+
+    public static <T1, T2> Pair<T1, T2> create(final T1 first, final T2 second) {
+	return new Pair<T1, T2>(first, second);
+    }
+
+    protected Pair(final T1 firstValue, final T2 secondValue) {
+	super();
+	this.first = firstValue;
+	this.second = secondValue;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (this.getClass() != obj.getClass()) {
+	    return false;
+	}
+	final Pair<T1, T2> other = (Pair<T1, T2>) obj;
+	return Objects.equal(this.first, other.first)
+		&& Objects.equal(this.second, other.second);
+    }
+
+    @Override
+    public int hashCode() {
+	return Objects.hashCode(this.first, this.second);
+    }
+
+    @Override
+    public String toString() {
+	return "Pair<" + this.first.toString() + "," + this.second.toString()
+		+ ">";
+    }
+}

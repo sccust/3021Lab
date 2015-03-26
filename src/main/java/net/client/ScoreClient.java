030523c856e6449d25d525236d3379bc9ef663bf
guxiaodong1987 <guxiaodong1987@126.com>
2015-03-26 14:20:23 +0800

    client and server

---
+++ src/main/java/net/client/ScoreClient.java
@@ -0,0 +1,14 @@
+package net.client;
+
+/**
+ * 
+ * @author Xiaodong
+ *
+ */
+public class ScoreClient
+{
+   public static void main(String[] args)
+   {
+	 // TODO Auto-generated method stub
+   }
+}
---
+++ src/main/java/net/server/ScoreServer.java
@@ -0,0 +1,14 @@
+package net.server;
+
+/**
+ * 
+ * @author Xiaodong
+ *
+ */
+public class ScoreServer
+{
+   public static void main(String[] args)
+   {
+	 // TODO Auto-generated method stub
+   }
+}

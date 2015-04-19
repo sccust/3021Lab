f4f1f7b1938d884e2516173a441ca89a8be5a75e
guxiaodong1987 <guxiaodong1987@126.com>
2015-04-19 16:24:05 +0800

    a

---
+++ src/main/java/net/client/BlogClient.java
@@ -0,0 +1,30 @@
+package net.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class BlogClient
+{
+   public static final String IP="127.0.0.1";
+   public static final int PORT = 3021;
+   
+   public static void main(String[] args)
+   {
+	 
+	 
+	try
+	 {
+	   String post="";
+	   Socket clientSocket=new Socket("127.0.0.1",3021);
+	   PrintWriter out =new PrintWriter(clientSocket.getOutputStream(),true);
+	    out.println(post);
+	 } catch (IOException e)
+	 {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	 }
+   }
+}

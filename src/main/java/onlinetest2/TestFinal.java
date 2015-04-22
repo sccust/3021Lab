2e8ced86bb9e0c4362dd180f3bb55ee9d6ed3b36
YidaGoo <yida1682@gmail.com>
2015-04-22 22:42:58 +0800

    test

--- src/main/java/onlinetest2/TestFinal.java
+++ src/main/java/onlinetest2/TestFinal.java
@@ -7,37 +7,135 @@ package onlinetest2;
  */
 public class TestFinal {
 	public static void main(String[] args) {
-		int totalScore = 0;
+		double totalScore = 0;
 		String scoreDetail = "";
 
-		TreeNode tree = new TreeNode(12);
-		TreeNode ch1 = new TreeNode(34);
-		TreeNode ch2 = new TreeNode(57);
-		TreeNode ch12 = new TreeNode(44);
-		ch12.setRightChild(new TreeNode(123));
-		ch1.setLeftChild(new TreeNode(13));
-		ch1.setRightChild(ch12);
-		tree.setLeftChild(ch1);
-		tree.setRightChild(ch2);
-		System.out.println(tree.getLeftChild().getRightChild().getRightChild()
-				.getData()
-				+ "");
-		System.out.println("data:" + tree.toString());
+		try {
+			TreeNode node1 = new TreeNode(11);
+			TreeNode node2 = new TreeNode(22);
 
-		/** Q1(1) */
-		if (tree.getLeftChild().getRightChild().getRightChild().getData() == 123) {
-			totalScore += 2;
-			scoreDetail += "2 ";
-		} else
-			scoreDetail += "0 ";
+			if (node1.getData() == 11 && node2.getData() == 22) {
+				/** Q1(1): constructor & getData */
+				totalScore += 0.8;
+				scoreDetail += "Q1(1): constructor + getData(): 0.8\n";
+			} else {
+				scoreDetail += "Q1(1): constructor + getData(): 0\n";
+			}
 
-		/** Q1(2) */
-		/** Relax the criterion by adding trim() */
-		if (tree.toString().trim().equalsIgnoreCase("12 34 57 13 44 123")) {
-			totalScore += 2;
-			scoreDetail += "2 ";
-		} else
-			scoreDetail += "0 ";
+		} catch (Exception e) {
+
+		}
+
+		try {
+			TreeNode root = new TreeNode(11);
+			TreeNode leftnode = new TreeNode(22);
+			root.setLeftChild(leftnode);
+
+			if (root.getLeftChild().getData() == 22) {
+				/** Q1(1): setLeftChild, getLeftChild */
+				totalScore += 0.6;
+				scoreDetail += "Q1(1): setLeftChild() + getLeftChild(): 0.6\n";
+			} else {
+				scoreDetail += "Q1(1): setLeftChild() + getLeftChild(): 0\n";
+			}
+
+		} catch (Exception e) {
+
+		}
+
+		try {
+			TreeNode root = new TreeNode(11);
+			TreeNode rightnode = new TreeNode(33);
+			root.setRightChild(rightnode);
+
+			if (root.getRightChild().getData() == 33) {
+				/** Q1(1): setRightChild, getRightChild */
+				totalScore += 0.6;
+				scoreDetail += "Q1(1): setRightChild() + getRightChild: 0.6\n";
+			} else {
+				scoreDetail += "Q1(1): setRightChild() + getRightChild: 0\n";
+			}
+
+		} catch (Exception e) {
+
+		}
+
+		try {
+
+			TreeNode tree = new TreeNode(12);
+			TreeNode ch1 = new TreeNode(34);
+			TreeNode ch2 = new TreeNode(57);
+			TreeNode ch12 = new TreeNode(44);
+			ch12.setRightChild(new TreeNode(123));
+			ch1.setLeftChild(new TreeNode(13));
+			ch1.setRightChild(ch12);
+			tree.setLeftChild(ch1);
+			tree.setRightChild(ch2);
+
+			/** Q1(2) */
+			/** Relax the criterion by adding trim() */
+			if (tree.toString().trim().equalsIgnoreCase("12 34 57 13 44 123")) {
+				totalScore += 2;
+				scoreDetail += "Q1(2): 2\n";
+			} else
+				scoreDetail += "Q1(2): 0\n";
+		} catch (Exception e) {
+
+		}
+
+		try {
+			GeneralTreeNode<Double> doubleTree = new GeneralTreeNode<Double>(
+					0.05);
+			GeneralTreeNode<Character> charTree = new GeneralTreeNode<Character>(
+					'c');
+
+			if (doubleTree.getData().equals(0.05)
+					&& charTree.getData().equals('c')) {
+				/** Q2(1): constructor & getData */
+				totalScore += 0.8;
+				scoreDetail += "Q2(1): constructor + getData(): 0.8\n";
+			} else {
+				scoreDetail += "Q2(1): constructor + getData(): 0\n";
+			}
+
+		} catch (Exception e) {
+
+		}
+
+		try {
+			GeneralTreeNode<Student> stuTree = new GeneralTreeNode<Student>(
+					new Student("Amy", "f"));
+			stuTree.setLeftChild(new GeneralTreeNode<Student>(new Student(
+					"Tom", "m")));
+			if (stuTree.getLeftChild().getData()
+					.equals(new Student("Tom", "m"))) {
+				/** Q2(1): leftChild setter+getter */
+				totalScore += 0.6;
+				scoreDetail += "Q2(1): setLeftChild() + getLeftChild(): 0.6\n";
+			} else {
+				scoreDetail += "Q2(1): setLeftChild() + getLeftChild(): 0\n";
+			}
+
+		} catch (Exception e) {
+
+		}
+
+		try {
+			GeneralTreeNode<Teacher> teacTree = new GeneralTreeNode<Teacher>(
+					new Teacher("Tim", 3021));
+			teacTree.setRightChild(new GeneralTreeNode<Teacher>(new Teacher(
+					"Zhang", 0422)));
+			if (teacTree.getRightChild().getData()
+					.equals(new Teacher("Zhang", 0422))) {
+				/** Q2(1): rightChild setter+getter */
+				totalScore += 0.6;
+				scoreDetail += "Q2(1): setRightChild() + getRightChild(): 0.6\n";
+			} else {
+				scoreDetail += "Q2(1): setRightChild() + getRightChild(): 0\n";
+			}
+		} catch (Exception e) {
+
+		}
 
 		GeneralTreeNode<Double> doubTree = new GeneralTreeNode<Double>(0.05);
 		doubTree.setLeftChild(new GeneralTreeNode<Double>(0.07));
@@ -49,8 +147,6 @@ public class TestFinal {
 				.setLeftChild(new GeneralTreeNode<Double>(0.55));
 		doubTree.getRightChild().setRightChild(
 				new GeneralTreeNode<Double>(0.82));
-		doubTree.getRightChild().getRightChild()
-				.setRightChild(new GeneralTreeNode<Double>(0.99));
 
 		GeneralTreeNode<String> strTree = new GeneralTreeNode<String>(
 				"Comp3021");
@@ -60,7 +156,6 @@ public class TestFinal {
 				.setLeftChild(new GeneralTreeNode<String>("blog"));
 		strTree.getLeftChild()
 				.setRightChild(new GeneralTreeNode<String>("gui"));
-		System.out.println(strTree.getLeftChild().getRightChild().getData());
 
 		GeneralTreeNode<Student> stuTree = new GeneralTreeNode<Student>(
 				new Student("Amy", "m"));
@@ -94,52 +189,41 @@ public class TestFinal {
 				.setRightChild(
 						new GeneralTreeNode<Teacher>(new Teacher("Wang", 2222)));
 
-		/** Q2(1) */
-		if (doubTree.getRightChild().getRightChild().getRightChild().getData()
-				.equals(0.99)
-				&& stuTree.getLeftChild().getRightChild().getData()
-						.equals(new Student("Kim", "f"))
-				&& teacTree.getRightChild().getLeftChild().getRightChild()
-						.getData().equals(new Teacher("Wang", 2222))) {
-			totalScore += 2;
-			scoreDetail += "2 ";
-		} else
-			scoreDetail += "0 ";
+		try {
+			/** Q2(2) */
+			boolean doubExist = doubTree.search(0.01);
+			boolean strExist = strTree.search("blog");
+			boolean stuExist = stuTree.search(new Student("Kim", "m"));
+			boolean teacExist = teacTree.search(new Teacher("Wang", 2222));
+			if (!doubExist && strExist && !stuExist && teacExist) {
+				totalScore += 2;
+				scoreDetail += "Q2(2): 2\n";
+			} else
+				scoreDetail += "Q2(2): 0\n";
+		} catch (Exception e) {
 
-		/** Q2(2) */
-		boolean doubExist = doubTree.search(0.01);
-		boolean strExist = strTree.search("blog");
-		boolean stuExist = stuTree.search(new Student("Kim", "m"));
-		boolean teacExist = teacTree.search(new Teacher("Wang", 2222));
-		System.out.println(doubExist);
-		System.out.println(strExist);
-		System.out.println(stuExist);
-		System.out.println(teacExist);
-		if (!doubExist && strExist && !stuExist && teacExist) {
-			totalScore += 2;
-			scoreDetail += "2 ";
-		} else
-			scoreDetail += "0 ";
+		}
 
-		/** Q2(3) */
-		boolean doubSymmetric = doubTree.isSymmetric();
-		boolean strSymmetric = strTree.isSymmetric();
-		boolean stuSymmetric = stuTree.isSymmetric();
-		boolean teacSymmetric = teacTree.isSymmetric();
-		System.out.println(doubSymmetric);
-		System.out.println(strSymmetric);
-		System.out.println(stuSymmetric);
-		System.out.println(teacSymmetric);
-		if (!doubSymmetric && !strSymmetric && stuSymmetric && !teacSymmetric) {
-			totalScore += 2;
-			scoreDetail += "2 ";
-		} else
-			scoreDetail += "0 ";
+		try {
+			/** Q2(3) */
+			boolean doubSymmetric = doubTree.isSymmetric();
+			boolean strSymmetric = strTree.isSymmetric();
+			boolean stuSymmetric = stuTree.isSymmetric();
+			boolean teacSymmetric = teacTree.isSymmetric();
+
+			if (!doubSymmetric && !strSymmetric && stuSymmetric
+					&& !teacSymmetric) {
+				totalScore += 2;
+				scoreDetail += "Q2(3): 2\n";
+			} else
+				scoreDetail += "Q2(3): 0\n";
+		} catch (Exception e) {
+
+		}
 
 		System.out.println("TotalScore:" + totalScore);
-		System.out.println("details:" + scoreDetail);
+		System.out.println("Details:\n" + scoreDetail);
 	}
-
 }
 
 class Student {
